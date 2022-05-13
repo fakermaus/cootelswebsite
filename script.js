37a2b370dde5ffa70268e690714f4bfc96e51af3
fakermaus <58190093+fakermaus@users.noreply.github.com>
2022-05-13 12:33:10 +0300

    my first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+new Swiper ('.swiper' , {
+
+    loop:true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dymanicBullets: true,
+    },
+
+    keyboard: {
+        enable: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+})
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
